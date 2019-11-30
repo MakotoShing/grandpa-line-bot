fc439480f5baf0c3c754654f4c999fdfa0832e58
MakotoShing <makotos1004@outlook.jp>
2019-12-01 00:08:00 +0900

    First Commit

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def handle_message(event):
             TextSendMessage(text=response.one_word()))
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="test"))
+            TextSendMessage(text="test1"))
 
 
 if __name__ == "__main__":
