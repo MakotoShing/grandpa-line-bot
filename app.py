5b81b5fd3397adf6dbf4eaeb280a3641ff044182
MakotoShing <makotos1004@outlook.jp>
2019-12-02 21:03:44 +0900

    Fix on a part of calculating days

--- app.py
+++ app.py
@@ -71,8 +71,8 @@ def handle_message(event):
             with open("Constant.json", "r") as fr:
                 Constant = json.load(fr)
             prev_date = datetime.datetime.strptime(Constant["SENT_DATE"], "%Y/%m/%d %H:%M:%S")
-            duration = datetime.datetime.now() - prev_date
-            if (duration // datetime.timedelta(days=1)) > 0:
+            days = datetime.datetime.now().day - prev_date.day
+            if days > 0:
                 line_bot_api.reply_message(
                     event.reply_token,
                     [TextSendMessage(text=response.one_word()),
